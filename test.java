4b7ba161473a4eedb9deedc5b543be55eb6ad2a8
Joe <87945079+Numb11@users.noreply.github.com>
2023-10-03 11:38:14 +0100

    Create test.java

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class test {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        Scanner scanObj = new Scanner(System.in);
+        System.out.println("Enter your name:");
+
+        String name = scanObj.nextLine();
+        System.out.println("Your name is" + " " + name);
+    }
+    
+}
+
+//Attempting to learn inputs
